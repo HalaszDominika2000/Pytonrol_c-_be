dd5fabb46815b2ba8da7ecf2ba4d9f0e89141f43
HalaszDominika2000 <duduka.urban@gmail.com>
2025-10-18 17:05:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //udvozles(); /*Metodus meghivasa*/
+        //hengerszamitas();
+        //szamBekeres();
+        primszamE();
+    }
+    static void udvozles() /*Ez a metodus*/
+    {
+        /*1. Feladat*/
+        Console.Write("Kérlek, add meg a neved: ");
+        string nev = Console.ReadLine();
+
+        Console.WriteLine($"Üdvözöllek, {nev}!"); /* Line az a sortörés ebben a parancsban: WriteLine*/
+
+    }
+    static void hengerszamitas()
+    {
+        /*Double törtszam*/
+        Console.Write("Add meg a henger sugarát (r): ");
+        /*Console.Readline string tipusu ezért át kell alakítani konvertálni a kívánt típusra azaz double-ra. To: val, vel*/
+        double r = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Add meg a henger magasságát (h): ");
+        double h = Convert.ToDouble(Console.ReadLine());
+        /*Math.Pow(x,2) az x-et négyzetre emeli ez a fuggveny 3 is lehet ez köbre emeli*/
+        double felszin = 2 * Math.PI * r * h + 2 * Math.PI * Math.Pow(r, 2);
+        double terfogat = Math.PI * Math.Pow(r, 2) * h;
+        Console.WriteLine($"A henger felszine: {felszin:F2}, a henger térfogata: {terfogat:F2}:  ");
+    }
+    static void szamBekeres()
+    {
+        Console.Write("Adj meg egy számot: ");
+        int szam = Convert.ToInt32(Console.ReadLine());
+        if (szam % 7 == 0 && szam % 11 == 0 && szam % 13 == 0)
+        {
+            Console.Write("A szám oszthato 7-tel 11-gyel és 13-mal is.");
+        }
+        else if (szam % 7 == 0)
+        {
+            Console.Write("A szám oszthato 7-tel.");
+        }
+        else if (szam % 11 == 0)
+        {
+            Console.Write("A szám oszthato 11-gyel.");
+        }
+        else if (szam % 13 == 0)
+        {
+            Console.Write("yA szám oszthato 13-mal.");
+        }
+        else
+        {
+            Console.Write("A szám nem oszthato sem 7-tel 11-gyel és 13-mal.");
+        }
+    }
+    static void primszamE()
+    {
+        
+    }
+}
